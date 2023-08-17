ce5ff0ac75caff46453c4950cbb74ff51c99db3a
nicolasceccato <nicolasceccato@hotmail.com>
2023-08-17 08:05:09 -0300

    utilizando lombok na classe Usuario

--- back/projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
+++ back/projeto/src/main/java/br/com/criandoapi/projeto/model/Usuario.java
@@ -1,6 +1,12 @@
 package br.com.criandoapi.projeto.model;
 
 import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 
 @Entity
 @Table(name = "usuario")
@@ -21,63 +27,4 @@ public class Usuario {
     private String telefone;
 
 
-    public Usuario() {
-    }
-
-    public Usuario(Integer id, String nome, String username, String email, String senha, String telefone) {
-        this.id = id;
-        this.nome = nome;
-        this.username = username;
-        this.email = email;
-        this.senha = senha;
-        this.telefone = telefone;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
