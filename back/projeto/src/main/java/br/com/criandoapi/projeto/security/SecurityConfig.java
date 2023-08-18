cec982f5d4d0a83027a19ecf7b652b7f4c8604d0
nicolasceccato <nicolasceccato@hotmail.com>
2023-08-18 18:21:34 -0300

    adicionando securuty filter

--- back/projeto/src/main/java/br/com/criandoapi/projeto/security/SecurityConfig.java
+++ back/projeto/src/main/java/br/com/criandoapi/projeto/security/SecurityConfig.java
@@ -1,19 +1,44 @@
 package br.com.criandoapi.projeto.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig  {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz.anyRequest()
-                                                   .authenticated())
-                                                   .httpBasic(withDefaults());
+//        http.csrf().disable()
+//                .authorizeHttpRequests()
+//                .requestMatchers(HttpMethod.POST, "/users/login").permitAll()
+//                .anyRequest().authenticated().and().cors();
+        http
+                .authorizeRequests()
+                .requestMatchers("/usuarios")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .cors();
         return http.build();
     }
+
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return null;
+    }
+
 }
